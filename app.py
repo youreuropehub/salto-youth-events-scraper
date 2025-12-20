aff9c7e67fcc505a74bde49fbe40b5a2e74664cb
YourEuropeHub <154826692+YourEuropeHub@users.noreply.github.com>
2025-12-20 12:55:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -353,4 +353,7 @@ def download_csv():
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
+
